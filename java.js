7310e3ad1bce9bc3b0fc86bc973795bf6e55c3a4
m1czi <43167167+m1czi@users.noreply.github.com>
2020-10-21 12:10:25 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
 function nazwa(){
     document.getElementById("gurom").innerHTML = "POLSKA GUROM";
 }
+function myFunction() {
+   document.querySelector(".container").style.background = "linear-gradient(0deg, rgba(0,255,21,1) 0%, rgba(254,0,253,1) 100%)";
+  }
